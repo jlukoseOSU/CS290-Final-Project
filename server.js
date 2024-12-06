dafff476506240b5e4399de7fc9eb9054e40700f
jlukoseOSU <lukosej@oregonstate.edu>
2024-12-05 18:46:06 -0800

    added express-handlebars

---
+++ server.js
@@ -0,0 +1,10 @@
+var path = require('path')
+var express = require('express')
+var { engine } = require('express-handlebars');
+  
+var app = express()
+var port = process.env.PORT || 3000
+
+app.listen(port, function () {
+    console.log("== Server is listening on port", port)
+})
